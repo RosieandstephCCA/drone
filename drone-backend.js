7bff4cd40d8097073e1520a4e9874bc00b538766
V3499 (Conference ID) <v3499@cam.ac.uk>
2015-08-14 10:48:00 +0100

    finished module 1 changes.

--- drone-backend.js
+++ drone-backend.js
@@ -11,11 +11,32 @@ Cylon.robot()
         driver: "ardrone",
         connection: "ardrone"
     })
+    .device("nav", {
+        driver: "ardrone-nav",
+        connection: "ardrone"
+    })
     .on("ready", fly);
     
 // Fly the bot
+
+
 function fly(robot) {
+    function fly(robot) {
+        bot = robot;
+        bot.drone.disableEmergency();
+        bot.drone.ftrim();
+        bot.drone.takeoff();
+        after(10*1000, function() {
+            bot.drone.left(0.2);
+        });
+        after(5 * 1000, function () {
+            bot.drone.left(0);
+            bot.drone.forward(0.2);
+        });
+        after(5 * 1000, function () {
+            bot.drone.forward(0);
+            bot.drone.stop();
+        });
+    }}
+    Cylon.start();
 
-}
-
-Cylon.start();
